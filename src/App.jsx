9fa0315d9fa7eb5d3c035b14ef7523a71222d6cb
nikhilShakya7 <rsnikhil77@gmail.com>
2025-01-29 08:42:09 +0545

    Added CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import "./App.css";
+
 import { useState } from "react";
 const App = () => {
-  const [weight, setWeight] = useState(0);
-  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState();
+  const [height, setHeight] = useState();
   const [bmi, setBMI] = useState(" ");
   const [message, setMessage] = useState(" ");
 
@@ -22,25 +24,37 @@ const App = () => {
       }
     }
   };
+  const reset = () => {
+    setBMI("");
+    setHeight("");
+    setWeight("");
+    setMessage("");
+  };
+
   return (
     <div>
       <div className="container">
+        <h2>BMI CALCULATOR</h2>
         <form onSubmit={calculate}>
           <label>Weight</label>
           <input
             type="text"
-            placeholder="Enter Your Weight"
+            placeholder="Enter Your Weight (in lbs)"
             value={weight}
             onChange={(e) => setWeight(e.target.value)}
           ></input>
           <label>Height</label>
           <input
             type="height"
-            placeholder="Enter Your Height"
+            placeholder="Enter Your Height (in inches)"
             value={height}
             onChange={(e) => setHeight(e.target.value)}
           ></input>
           <button type="submit">Calculate BMI</button>
+          <button type="button" className="reset" onClick={reset}>
+            Reset
+          </button>
+
           <div className="">
             <h3>Your BMI is {bmi}</h3>
             <p>{message}</p>
